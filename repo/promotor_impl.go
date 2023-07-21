85d641e1a3af47321a37fd285c345cb56cc4ec69
Vincent Andreas <vincent.andreas89@gmail.com>
2023-07-21 16:19:04 +0700

    commit tgl 21 juli

--- repo/promotor_impl.go
+++ repo/promotor_impl.go
@@ -4,6 +4,7 @@ import (
 	"ticket-expert/models"
 )
 
+/// siapa punya duit bagi dong, dari masa lalu ini. .
 func (repo *Implementation) SavePromotor(promotor models.Promotor) {
 	repo.db.Create(&promotor)
 }
